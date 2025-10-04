ea95c696528d6cff15a92c16a20b30390055c65c
jofawebdev <webmaniac10@gmail.com>
2025-10-04 18:01:57 +0300

    Added Order History Functions

--- Base_App/admin.py
+++ Base_App/admin.py
@@ -14,9 +14,23 @@ class OfferAdmin(admin.ModelAdmin):
     is_currently_active.short_description = 'Currently Active'
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0  # No empty extra lines
+    readonly_fields = ('item_name', 'original_price', 'final_price')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'customer_name', 'final_total', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    inlines = [OrderItemInline]
+    readonly_fields = ('order_number', 'original_total', 'discount_amount', 'final_total')
+
+
 admin.site.register(ItemList)
 admin.site.register(Items)
 admin.site.register(Offer, OfferAdmin)
 admin.site.register(AboutUs)
 admin.site.register(Feedback)
 admin.site.register(BookTable)
+admin.site.register(OrderItem)
